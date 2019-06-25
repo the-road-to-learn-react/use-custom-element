3a6cb338312474b016e918e733d84134f181aa20
Robin Wieruch <wrobin@gmx.net>
2019-06-25 10:09:42 +0200

    escape if there is no ref.current

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ const useCustomElement = (props, customMapping = {}) => {
   const ref = React.createRef();
 
   React.useLayoutEffect(() => {
-    const fns = Object.keys(props)
-      .filter(key => props[key] instanceof Function)
-      .map(key => ({
-        key: customMapping[key] || key,
-        fn: customEvent =>
-          props[key](customEvent.detail, customEvent),
-      }));
+    let fns;
 
-    fns.forEach(({ key, fn }) =>
-      ref.current.addEventListener(key, fn),
-    );
+    if (ref.current) {
+      fns = Object.keys(props)
+        .filter(key => props[key] instanceof Function)
+        .map(key => ({
+          key: customMapping[key] || key,
+          fn: customEvent =>
+            props[key](customEvent.detail, customEvent),
+        }));
+
+      fns.forEach(({ key, fn }) =>
+        ref.current.addEventListener(key, fn),
+      );
+    }
 
     return () =>
+      ref.current &&
       fns.forEach(({ key, fn }) =>
         ref.current.removeEventListener(key, fn),
       );
