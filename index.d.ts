21f3c36f012a9b9b1677c3fceb69c1dbcd2eb482
ShaMan123 <shacharnen@gmail.com>
2020-12-20 14:44:03 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 ///// <reference path="react" />
 
-export default function useCustomElement<R extends HTMLElement, P>(
+export default function useCustomElement<R extends HTMLElement, P = {}>(
     props: P,
     customMapping: Partial<{ [key in P]: string }> = {}
 ): [Partial<{ [key in P]: string }>, React.RefObject<R>]
