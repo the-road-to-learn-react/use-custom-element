b221fe61501a734fab896d49b94b7ffad1011802
ShaMan123 <shacharnen@gmail.com>
2020-12-20 14:23:33 +0200

    accept react nodes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { renderToString } from 'react-dom/server';
 
 const useCustomElement = (props, customMapping = {}) => {
   const ref = React.useRef();
@@ -18,7 +19,7 @@ const useCustomElement = (props, customMapping = {}) => {
         }));
 
       fns.forEach(({ key, fn }) => {
-        current.addEventListener(key, fn)
+        current.addEventListener(key, fn);
       });
     }
 
@@ -35,14 +36,18 @@ const useCustomElement = (props, customMapping = {}) => {
     .filter(key => !(props[key] instanceof Function))
     .reduce((acc, key) => {
       const prop = props[key];
-
       const computedKey = customMapping[key] || key;
+      let value;
 
-      if (prop instanceof Object || prop instanceof Array) {
-        return { ...acc, [computedKey]: JSON.stringify(prop) };
+      if (React.isValidElement(prop) || (prop instanceof Array && prop.every(React.isValidElement))) {
+        value = renderToString(prop);
+      } else if (prop instanceof Object || prop instanceof Array) {
+        value = JSON.stringify(prop);
+      } else {
+        value = prop;
       }
 
-      return { ...acc, [computedKey]: prop };
+      return { ...acc, [computedKey]: value };
     }, {});
 
   return [customElementProps, ref];
